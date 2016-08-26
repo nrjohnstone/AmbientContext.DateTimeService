9fa311cd91c34ab8d4e20093c81411e04a94fc02
nrjohnstone <locotewz@gmail.com>
2016-08-26 06:39:57 +0200

    implementation of AmbientDateTimeService

---
+++ src/AmbientContext.DateTimeService/DateTimeAdapter.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AmbientContext.DateTimeService
+{
+    public class DateTimeAdapter : IDateTime
+    {
+        public DateTime Now => DateTime.Now;
+        public DateTime UtcNow => DateTime.UtcNow;
+        public DateTime Today => DateTime.Today;
+    }
+}

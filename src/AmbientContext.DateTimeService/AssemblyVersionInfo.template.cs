59bc501b30e7368cdb3fb016d3624c4a001e2e06
nrjohnstone <locotewz@gmail.com>
2017-03-29 12:35:04 +0200

    remove AssemblyVersion attribute

--- src/AmbientContext.DateTimeService/AssemblyVersionInfo.template.cs
+++ src/AmbientContext.DateTimeService/AssemblyVersionInfo.template.cs
@@ -2,6 +2,5 @@ using System.Reflection;
 
 // GENERATED FILE - DO NOT EDIT
 
-[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyInformationalVersion("0.1.0-initialbuild")]
 [assembly: AssemblyFileVersion("0.1.0.0")]

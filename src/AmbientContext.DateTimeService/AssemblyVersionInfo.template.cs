6ed46356f753ac5028347b1db85c1126730765d9
nrjohnstone <locotewz@gmail.com>
2017-03-29 12:26:22 +0200

    add template version information file

---
+++ src/AmbientContext.DateTimeService/AssemblyVersionInfo.template.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+// GENERATED FILE - DO NOT EDIT
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-initialbuild")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
